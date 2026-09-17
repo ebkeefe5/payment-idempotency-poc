d6545b95600e922f751dc3efe1371f97f9033303
ebkeefe5 <ebkeefe5@gmail.com>
2026-09-17 08:56:14 -0600

    Generate bakend with start.spring.io

---
+++ backend/src/main/java/com/payments/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
